2151a02a42634660009d53e50207ce62e0f1ed9d
Andreas Robecke <a.robecke@getsafe.de>
2019-04-28 18:36:25 +0200

    Indentation

--- spec/cassette_name_by_example_spec.rb
+++ spec/cassette_name_by_example_spec.rb
@@ -2,15 +2,21 @@ RSpec.describe NxtVcrHarness::CassetteNameByExample do
   describe '#call' do
     context 'when there is a context', :vcr_cassette do
       it 'names the file appropriately' do |example|
-        result = described_class.new(example).call
-        expect(result).to match(/\A\/cassette_name_by_example_spec\/NxtVcrHarness::CassetteNameByExample\/call\/when_there_is_a_context\z/)
+        expect(
+          described_class.new(example).call
+        ).to match(
+          /\A\/cassette_name_by_example_spec\/NxtVcrHarness::CassetteNameByExample\/call\/when_there_is_a_context\z/
+        )
       end
     end
 
     context 'with an it block' do
       it 'names the file appropriately', :vcr_cassette do |example|
-        result = described_class.new(example).call
-        expect(result).to match(/\A\/cassette_name_by_example_spec\/NxtVcrHarness::CassetteNameByExample\/call\/with_an_it_block\/names_the_file_appropriately\z/)
+        expect(
+          described_class.new(example).call
+        ).to match(
+          /\A\/cassette_name_by_example_spec\/NxtVcrHarness::CassetteNameByExample\/call\/with_an_it_block\/names_the_file_appropriately\z/
+        )
       end
     end
   end
